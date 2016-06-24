4b8f9ac7fbd05a7333016c36fcfcef407b0502c5
EA4GKQ <angel@ayasystems.com>
2016-06-24 22:52:53 +0200

    Create status.php

---
+++ status.php
@@ -0,0 +1,25 @@
+<?php 
+
+include "./config/config.php";
+include './RelayLib.php';
+
+try {
+
+
+	$lib = new RelayLib(IP, PORT, USER, PASSWORD);
+	$status = $lib->status();
+	$name   = $lib->name();
+	$complete = array_combine($name, $status);
+  echo'{"Status":'.json_encode($complete).'}'; 
+     
+}catch (Exception $exc) {
+
+	echo $exc->getMessage().'<br/>';
+	echo $exc->getTraceAsString();
+	die();
+
+}
+
+
+?>
+     
